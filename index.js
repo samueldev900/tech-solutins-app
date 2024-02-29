35d57ddb64b409e00f464e3d47819d5df0f03ee7
Samuel <samueloliveira900@gmail.com>
2024-02-29 15:36:21 -0300

    inciado o script js e height das paginas ajustado

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+let sections = document.querySelectorAll('.section')
+let links = document.querySelectorAll(".js-link");
+window.addEventListener('scroll',()=>{
+    
+    sections.forEach(section =>{
+
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let heightSection = section.offsetHeight;
+        let idSection =section.getAttribute('id');
+
+        if(top >= offset && top < offset + heightSection){
+
+
+
+        }
+        console.log(heightSection)
+    })
+
+
+})
+
