fb0357da8b87a513f7a002af3267444653c86edd
Samuel <samueloliveira900@gmail.com>
2024-03-31 17:03:24 -0300

    conexão com banco de Dados AWS com sucesso

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "techsolutionsdb2.cnya4ws6ctnq.us-east-2.rds.amazonaws.com";
+$username = "samueloliveira";
+$password = "rWrzb763Jeg8bSJvZ0xj";
+$database = "techsolutionsdb2";
 
 $conn = new mysqli($servername,$username,$password);
 
-if($conn->connect_error){
-
-    die("A conexão falhou: " . $conn->connect_error);
+if(!$conn){
+    echo "Não foi possivel conectar";
 }
